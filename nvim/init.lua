0ee0ce80fa0f041e0ca84fbe2c296430f41cc44c
keym <20496472+sudowoodo-keym@users.noreply.github.com>
2026-06-13 13:50:49 -0400

    rolled back dark theme switch

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = " "
 
 -- Colorscheme Auto Mode
 local hour = tonumber(os.date("%H"))
-if hour >= 8 and hour < 20 then
+if hour >= 8 and hour < 17 then
   vim.o.background = "light"
   vim.api.nvim_set_hl(0, 'InsertCursor', {
     fg = "#f0f0f0",
